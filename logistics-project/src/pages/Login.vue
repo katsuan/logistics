6f0d302e16a094fdbb1144c1ec79a222a3169508
katsushima <>
2024-03-29 16:45:27 +0900

    [add]郵便番号の規則を追加

--- logistics-project/src/pages/Login.vue
+++ logistics-project/src/pages/Login.vue
@@ -8,21 +8,45 @@ const form = reactive({
     address: null
 })
 
-const url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=";
+const baseUrl = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=";
 const getData = async () => {
-    let result = await axios.get(url + String(form.postcode));
-    const address = result.data.results[0]
-    console.log(address.address1 + address.address2 + address.address3);
-    form.address = address.address1 + address.address2 + address.address3
-    return address.address1 + address.address2 + address.address3;
+    if (String(form.postcode).length < 7) {
+        form.address = ''
+        return
+    }
+    let result = await axios.get(baseUrl + String(form.postcode));
+    console.log(result.data);
+    if (result.data.results === null) {
+        form.address = '存在しない郵便番号です'
+        return
+    } else {
+        const address = result.data.results[0]
+        console.log(address.address1 + address.address2 + address.address3);
+        form.address = address.address1 + address.address2 + address.address3
+        return address.address1 + address.address2 + address.address3;
+    }
 };
 
 </script>
 
 <template>
-    <span>Loginページの予定です</span>
-    <input type="number" name="postcode" @change="getData" v-model=form.postcode>
-    <input type="string" name="address" v-model=form.address>
+    <h1>住所入力</h1>
+    <div class="container">
+        <span>郵便番号</span>
+        <input type="number" name="postcode" @input="getData" v-model=form.postcode>
+        <span>住所</span>
+        <input type="string" class="address" name="address" v-model=form.address>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+}
+
+.address {
+    width: auto;
+}
+</style>
