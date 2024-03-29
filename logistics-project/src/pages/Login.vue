9c10cb9ff314bd64b532cb46f6e416f028f45baf
katsushima <>
2024-03-29 15:50:38 +0900

    [add]郵便番号APIのテスト

--- logistics-project/src/pages/Login.vue
+++ logistics-project/src/pages/Login.vue
@@ -1,10 +1,21 @@
-<script setup lang="ts">
+<script setup>
+import { reactive } from 'vue'
+import { Core as YubinbangoCore } from 'yubinbango-core2';
 
+const form = reactive({
+    postcode: null
+})
+
+const fetchAddress = () => {
+    new YubinbangoCore(String(form.postcode), (value) => {
+        console.log(value)
+    })
+}
 </script>
 
 <template>
     <span>Loginページの予定です</span>
+    <input type="number" name="postcode" @change="fetchAddress" v-model=form.postcode>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
