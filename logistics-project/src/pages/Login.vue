5d7736732a6fa49ecb972d11fd09eb8ffc219f2c
katsushima <>
2024-03-29 16:20:14 +0900

    [add]住所を動的にAPI変更

--- logistics-project/src/pages/Login.vue
+++ logistics-project/src/pages/Login.vue
@@ -1,21 +1,28 @@
 <script setup>
-import { reactive } from 'vue'
+import axios from "axios";
+import { reactive } from 'vue';
 import { Core as YubinbangoCore } from 'yubinbango-core2';
 
 const form = reactive({
-    postcode: null
+    postcode: null,
+    address: null
 })
 
-const fetchAddress = () => {
-    new YubinbangoCore(String(form.postcode), (value) => {
-        console.log(value)
-    })
-}
+const url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=";
+const getData = async () => {
+    let result = await axios.get(url + String(form.postcode));
+    const address = result.data.results[0]
+    console.log(address.address1 + address.address2 + address.address3);
+    form.address = address.address1 + address.address2 + address.address3
+    return address.address1 + address.address2 + address.address3;
+};
+
 </script>
 
 <template>
     <span>Loginページの予定です</span>
-    <input type="number" name="postcode" @change="fetchAddress" v-model=form.postcode>
+    <input type="number" name="postcode" @change="getData" v-model=form.postcode>
+    <input type="string" name="address" v-model=form.address>
 </template>
 
 <style scoped></style>
--- logistics-project/src/pages/Test.vue
+++ logistics-project/src/pages/Test.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts">
+<script setup>
+import axios from "axios";
+import { reactive } from "vue";
 
+const url = "http://localhost:8080/api/completions";
+
+defineProps({ ← App.vueからのProps
+    msg: String,
+})
+const data = reactive({ ← HelloWorld.vueで利用するreactiveオブジェクト
+    responses: "",
+    keyWord: "",
+});
+
+const getData = async () => {
+    let result = await axios.get(url, { params: { prompt: data.keyWord } }); ← サーバサイド呼び出し
+    data.responses = result.data;
+};
 </script>
 
 <template>
-    <span>Testページの予定です</span>
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="data.keyWord" @input="inputPrompt" />
+    <button @click="getData">Request</button>
+    <hr>
+    <div v-for="(item, index) in data.responses.choices">
+        {{ item.text }} ← サーバサイドから返却された値を表示する
+    </div>
 </template>
 
 <style scoped></style>
